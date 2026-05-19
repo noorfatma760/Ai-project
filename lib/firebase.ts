9b8b609167aad6d72cdc963f700ef467a4230084
noorfatma760 <noorfatma0573@gmail.com>
2026-05-19 21:04:49 +0500

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBdSOmpA8MQjvhFAyUQDVBsK9cp8m_sJ0M",
   authDomain: "card-game-84175.firebaseapp.com",
   projectId: "card-game-84175",
-  storageBucket: "card-game-84175.firebasestorage.app",
+  storageBucket: "card-game-84175.appspot.com",
   messagingSenderId: "890186103813",
   appId: "1:890186103813:web:3fa1b9e95c657a7ef4306c"
 };
